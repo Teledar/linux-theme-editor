78a0de72f0da87a8c1988323764037ca4c873250
Teledar <lmartin1920@outlook.com>
2025-10-26 16:39:06 -0400

    Use single object to manage styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import { useState } from 'react'
 import WindowSimulation from './components/WindowSimulation'
 
+const defaultWindowStyle = {
+  backgroundColor: 'white',
+}
+
+const defaultStyle = {
+  window: {
+    ...defaultWindowStyle,
+    borderTop: 'none',
+    borderLeft: 'solid 2px black',
+    borderRight: 'solid 2px black',
+    borderBottom: 'solid 2px black',
+  },
+  maximizedWindow: {
+    ...defaultWindowStyle,
+    borderTop: 'none',
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderBottom: 'none',
+  },
+  titleBar: {
+    backgroundColor: 'black',
+    height: 20,
+  },
+}
+
 const App = () => {
-  const [titleBarColor, setTitleBarColor] = useState('darkgray')
+  const [style, setStyle] = useState(defaultStyle)
 
   return (
     <>
       <h1>Linux Theme Editor</h1>
-      <WindowSimulation titleBarColor={titleBarColor} maximized={false} />
+      <WindowSimulation windowStyle={style.window} titleBarStyle={style.titleBar}
+      position={{ top:10, left:10, right:10, bottom:10 }} />
     </>
   )
 }
--- src/components/WindowSimulation.jsx
+++ src/components/WindowSimulation.jsx
@@ -1,20 +1,10 @@
-const defaultWindowBackground = 'white'
-const defaultWindowBorder = 'solid 2px black'
-const defaultMaximizedWindowBorder = 'none'
-const defaultTitleBarColor = 'black'
-const defaultTitleBarHeight = 20
-
 const TitleBar = (props) => {
-  const background = props.background ? props.background : defaultTitleBarColor
-  const height = props.height ? props.height : defaultTitleBarHeight
-
   const style = {
+    ...props.style,
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    backgroundColor: background,
-    height: height,
   }
 
   return (
@@ -25,46 +15,23 @@ const TitleBar = (props) => {
 } 
 
 const WindowSimulation = (props) => {
-  const background = props.background ? props.background : defaultWindowBackground
-
   const style = {
+    ...props.position,
     position: 'fixed',
   }
 
   const contentStyle = {
+    ...props.windowStyle,
     position: 'absolute',
-    top: props.titleBarHeight ? props.titleBarHeight : defaultTitleBarHeight,
+    top: props.titleBarStyle.height,
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: background,
-    borderTopStyle: 'none',
-  }
-
-  if (props.maximized) {
-    style.top = 0
-    style.left = 0
-    style.right = 0
-    style.bottom = 0
-    const border = props.border ? props.border : defaultMaximizedWindowBorder
-    contentStyle.borderLeft = border
-    contentStyle.borderRight = border
-    contentStyle.borderBottom = border
-  }
-  else {
-    style.top = 10
-    style.left = 10
-    style.right = 10
-    style.bottom = 10
-    const border = props.border ? props.border : defaultWindowBorder
-    contentStyle.borderLeft = border
-    contentStyle.borderRight = border
-    contentStyle.borderBottom = border
   }
 
   return (
     <div style={style}>
-      <TitleBar background={props.titleBarColor} height={props.titleBarHeight} />
+      <TitleBar style={props.titleBarStyle} />
       <div style={contentStyle}>
 
       </div>
